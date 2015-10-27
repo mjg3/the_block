2eee398029d5141f84b2f66b94bd7d168129cd4b
Kelly Howard <kellybhoward@gmail.com>
2015-10-27 16:05:24 -0700

    added auction model, changed home view to auction view, added constructor function to auctions controller

--- AuctionInstant/application/controllers/auctions.php
+++ AuctionInstant/application/controllers/auctions.php
@@ -2,6 +2,13 @@
 
 class Auctions extends CI_Controller {
 
+	public function __construct()
+	{
+		parent:: __construct();
+		// $this->output->enable_profiler(TRUE);
+		$this->load->model("Auction");
+	}
+
 	public function index() {
 		if($this->session->userdata('logged_in') == true)
 		{
---
+++ AuctionInstant/application/models/auction.php
@@ -0,0 +1,29 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Model extends CI_Model {
+
+	public function __construct(){
+		parent:: __construct();
+	}
+
+	public function all()
+	{
+		return $this->db->query("")->result_array();
+	}
+
+	public function create()
+	{
+
+	}
+
+	public function delete($id)
+	{
+		$this->db->query("DELETE FROM  WHERE id=?", $id);
+	}
+
+	public function update()
+	{
+
+	}
+}
+?>
