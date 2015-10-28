457d5aeb5f0472b175b1fd83aae913d217c0dd05
Kelly Howard <kellybhoward@gmail.com>
2015-10-27 17:13:45 -0700

    added reg/log validation and session data

--- AuctionInstant/application/controllers/auctions.php
+++ AuctionInstant/application/controllers/auctions.php
@@ -19,6 +19,78 @@
 				$this->load->view('login');
 			}
 		}
+
+		public function logout()
+		{
+			$this->session->sess_destroy();
+			redirect("/");
+		}
+
+		public function login()
+		{
+			$result = $this->user->validate_login($this->input->post());
+
+		    if($result == "valid")
+		    {
+		    	$user_info = ['user_info'=>$this->user->get_user_by_email($this->input->post("email"))];
+		    	$this->session->set_userdata('user_id', $user_info['user_info']['user_id']);
+		    	$this->session->set_userdata('logged_in', true);
+		    	$this->session->set_userdata('email', $user_info['user_info']['email']);
+		    	redirect("/");
+		    }
+			else
+			{
+		    	$errors = array(validation_errors());
+		    	$this->session->set_flashdata('errors', $errors);
+		    	redirect("/");
+		    }
+		}
+
+		public function register()
+		{
+		    $result = $this->user->validate_registration($this->input->post());
+
+		    if($result == "valid")
+		    {
+				$first_name = $this->input->post("first_name");
+				$last_name = $this->input->post("last_name");
+				$email = $this->input->post("email");
+				$password = md5($this->input->post("password"));
+
+				$user_info = ["first_name"=>$first_name, "last_name"=>$last_name, "email"=>$email, "password"=>$password];
+
+				$this->user->add_user($user_info);
+				$user_info = ['user_info' =>$this->user->get_user_by_email($this->input->post("email"))];
+				$this->session->set_userdata('logged_in', true);
+				$this->session->set_userdata('user_id', $user_info['user_info']['user_id']);
+				$this->session->set_userdata('email', $user_info['user_info']['email']);
+		    	redirect('/');
+		    }
+		    else{
+		    	$errors = array(validation_errors());
+		    	$this->session->set_flashdata('errors', $errors);
+		    	redirect("/");
+		    }
+		}
+		
+		function check_database($email)
+		{
+		    $email = $this->input->post('email');
+		    $password = md5($this->input->post('password'));
+
+		    $result = $this->user->login($email, $password);
+
+		    if($result)
+		    {
+		    	return TRUE;
+		    }
+		    else
+		    {
+		    	$this->form_validation->set_message('check_database', 'Invalid username or password');
+		    	return false;
+		    }
+		}
+
 	}
 
 ?>
--- AuctionInstant/application/models/auction.php
+++ AuctionInstant/application/models/auction.php
@@ -6,24 +6,62 @@
 			parent:: __construct();
 		}
 
-		public function all()
+		//validate registration
+		public function validate_registration($post)
 		{
-			
+			$this->load->helper(array('form', 'url'));
+		    $this->load->library('form_validation');
+		    $this->form_validation->set_rules('first_name', 'First Name', 'trim|min_length[3]|alpha_dash|required|xss_clean');
+		    $this->form_validation->set_rules('last_name', 'Last Name', 'trim|min_length[3]|alpha_dash|required|xss_clean');
+		    $this->form_validation->set_rules('email', 'Email', 'trim|is_unique[users.email]|valid_email|required');
+		    $this->form_validation->set_rules('password', 'Password', 'trim|min_length[8]|matches[confirm_password]|required|md5');
+		    if($this->form_validation->run())
+		    {
+		    	return "valid";
+		    }
+		    else
+		    {
+		    	return array(validation_errors());
+		    }
 		}
 
-		public function create()
+		//validate login form
+		public function validate_login($login_info)
 		{
+			$this->load->helper(array('form', 'url'));
+		    $this->load->library('form_validation');
+		    $this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean|callback_check_database');
+		    $this->form_validation->set_rules('password', 'Password', 'trim|required|xss_clean');
 
+		    if($this->form_validation->run())
+		    {
+		    	return "valid";
+		    }
+		    else
+		    {
+		    	return array(validation_errors());
+		    }
 		}
 
-		public function delete($id)
+		//make sure user is unique and matches database before logging in
+		public function login($email, $password)
 		{
+		    $this -> db -> select('email', 'password', 'id');
+		    $this -> db -> from('users');
+		    $this -> db -> where('email', $email);
+		    $this -> db -> where('password', md5($password));
+		    $this -> db -> limit(1);
 
-		}
+		    $query = $this -> db -> get();
 
-		public function update()
-		{
-
+		    if($query -> num_rows() == 1)
+		    {
+		    	return $query->result();
+		    }
+		    else
+		    {
+		    	return false;
+		    }
 		}
 	}
 
